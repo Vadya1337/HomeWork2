7991c9ba781919dc810c47b424a044f7b4efc603
Вадим <vadim.filatov.02@gmail.com>
2024-08-14 01:53:05 +0700

    сделал дз

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,54 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+var dog = 8.0;
+System.out.println(dog);
+var cat = 3.6;
+System.out.println(cat);
+var paper = 763789;
+System.out.println(paper);
+var dogSumm = dog + 4;;
+        System.out.println(dogSumm);
+        var catSumm = cat + 4;
+        System.out.println(catSumm);
+        var paperSumm = paper + 4;
+        System.out.println(paperSumm);
+        var dogRaznost = dog - 3.5;
+        System.out.println(dogRaznost);
+        var catRaznost = cat - 1.6;
+        System.out.println(catRaznost);
+        var paperRaznost = paper - 7639;
+        System.out.println(paperRaznost);
+var friend = 19;
+System.out.println(friend);
+friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+         frog = frog * 10;
+        System.out.println(frog);
+         frog = frog / 3.5;
+        System.out.println(frog);
+         frog = frog + 4;
+        System.out.println(frog);
+var oneBoks = 78.2;
+var twoBoks = 82.7;
+var BoksSumm = oneBoks + twoBoks;
+System.out.println(" Суммарная масса боксеров "  + BoksSumm + " кг ");
+var BoksRazn = twoBoks - oneBoks;
+System.out.println(" Второй боксёр тяжелее первого на "  + BoksRazn + " кг ");
+var BoksOstatok = twoBoks % oneBoks;
+System.out.println(BoksOstatok);
+
+var vsegoHours = 640;
+var vsegoRabotnikov = vsegoHours / 8;
+System.out.println("Всего в компании работает - " + vsegoRabotnikov + " человек");
+var BolsheRabotnikov = vsegoRabotnikov + 94;
+var BolsheHours = BolsheRabotnikov * 8;
+System.out.println("Если в компании работает " + BolsheRabotnikov + " человек, то всего " + BolsheHours + " часов работы может быть поделено между работниками");
+
 
     }
 }
